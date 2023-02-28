c7ba7e1d56a9f149f657badc1e816971b667961f
probiss <bulut.cihad@gmail.com>
2023-02-28 11:45:05 +0300

    a few cars adde

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,36 @@
-// ESNEK
+exports.seed = async function(knex) {
+    await knex('cars').insert([
+        {
+            vin:'01', 
+            make:'Mitsubishi',
+            model:'Lancer',
+            mileage:'10000',
+            title:'Ölücüler Yazmasın',
+            transmission:'Manuel'
+        },
+        {
+            vin:'02', 
+            make:'BMW',
+            model:'M5',
+            mileage:'35000',
+            title:'Dont Cry',
+            transmission:'Otomatik'
+        },
+        {
+            vin:'03', 
+            make:'Audi',
+            model:'A5',
+            mileage:'67000',
+            title:'Favorin Sen de Biliyorsun',
+            transmission:'Otomatik'
+        },
+        {
+            vin:'04', 
+            make:'Seat',
+            model:'Leon',
+            mileage:'98000',
+            title:'Hatıralar...',
+            transmission:'Otomatik'
+        }
+    ]);
+};
